c5dd1642b73262957929eed04bba8154061a6fb3
MordilloSan <miguelgalizamariz@gmail.com>
2025-10-11 12:15:21 +0100

    socket determination update enviorment variables removal C helper update

--- backend/common/session/socket.go
+++ backend/common/session/socket.go
@@ -1,13 +1,22 @@
+// session/socket.go
 package session
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"path/filepath"
+	"strconv"
 )
 
 const RuntimeBase = "/run/linuxio"
 
-func (s *Session) SocketPath() string {
-	name := fmt.Sprintf("linuxio-bridge-%s.sock", s.SessionID[:24])
-	return filepath.Join(RuntimeBase, s.User.UID, name)
+func SocketPath(uid uint32) (string, error) {
+	var b [16]byte
+	if _, err := rand.Read(b[:]); err != nil {
+		return "", err
+	}
+	token := hex.EncodeToString(b[:]) // 32 hex chars
+	dir := filepath.Join(RuntimeBase, strconv.FormatUint(uint64(uid), 10))
+	return filepath.Join(dir, fmt.Sprintf("linuxio-bridge-%s.sock", token)), nil
 }
